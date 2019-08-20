e2d16e24312931882f0e9ea5ffc0c0b2109f2a84
perlancar (on pc-office) <perlancar@gmail.com>
2019-08-20 17:14:41 +0700

    Initial commit

---
+++ lib/App/TermAttrUtils.pm
@@ -0,0 +1,19 @@
+package App::TermAttrUtils;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: CLI utilities related to querying terminal attributes
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities related to
+querying terminal attributes (color support, size, other capabilities):
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+B<terminfo>, L<Term::Terminfo>
